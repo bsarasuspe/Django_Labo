281a20f32888368a733ca705d8efd8cef2967811
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-28 09:37:45 +0200

    Agregar archivos de proyecto.

---
+++ Django_Labo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bsara\Desktop\Unibertsitatea\3. Maila\SGTA\Django_Labo\Django_Labo\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
