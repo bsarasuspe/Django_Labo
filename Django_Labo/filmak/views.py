334e29f52c2578a5fb60ebfc65f5c47be5ff461b
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-28 10:33:01 +0200

    index / login / register

--- Django_Labo/filmak/views.py
+++ Django_Labo/filmak/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return render(request, "filmak/index.html",
+                  {
+                      'title' : "Hello Django",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
