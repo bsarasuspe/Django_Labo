efcf38377cfa697a7b8651166745d28b3b9df6f6
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-28 09:44:31 +0200

    Filmak aplikazioa + models.py

---
+++ Django_Labo/filmak/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+
+class Filma(models.Model):
+	izenburua = models.CharField(max_length=60)
+	zuzendaria = models.CharField(max_length=60)
+	urtea = models.IntegerField(max_length=11)
+	generoa = models.CharField(max_length=2)
+	sinopsia = models.CharField(max_length=700)
+	bozkak = models.IntegerField(max_length=11)
+
+	def __unicode__(self):
+		return self.izenburua
+
+
+class Bozkatzailea(models.Model):
+	erabiltzailea_id = models.ForeignKey(User)
+	gogokofilmak = models.ManyToManyField(Filma)
+
+	def __unicode__(self):
+		return unicode(self.erabiltzailea_id)
