93e978a3900bf47389ab0be9cec11ef0c9e7e7b0
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-30 11:10:04 +0200

    Beharrezko orriak sortu

--- Django_Labo/filmak/urls.py
+++ Django_Labo/filmak/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('menua', views.menua, name='menua'),
+    path('bozkatu', views.bozkatu, name='bozkatu'),
+    path('zaleak', views.zaleak, name='zaleak'),
 ]
--- Django_Labo/filmak/views.py
+++ Django_Labo/filmak/views.py
@@ -6,7 +6,67 @@ def index(request):
 
     return render(request, "filmak/index.html",
                   {
-                      'title' : "Hello Django",
+                      'title' : "Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def login(request):
+
+    return render(request, "filmak/login.html",
+                  {
+                      'title' : "Login - Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def register(request):
+
+    return render(request, "filmak/register.html",
+                  {
+                      'title' : "Register - Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def logout(request):
+
+    return render(request, "filmak/logout.html",
+                  {
+                      'title' : "Logout - Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def menua(request):
+
+    return render(request, "filmak/menua.html",
+                  {
+                      'title' : "Menua - Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def bozkatu(request):
+
+    return render(request, "filmak/bozkatu.html",
+                  {
+                      'title' : "Bozkatu - Filmak",
+                      'message' : "Hello Django!",
+                      'content' : "Hello, world. You're at the polls index."
+                      }
+                  )
+
+def zaleak(request):
+
+    return render(request, "filmak/zaleak.html",
+                  {
+                      'title' : "Zaleak - Filmak",
                       'message' : "Hello Django!",
                       'content' : "Hello, world. You're at the polls index."
                       }
