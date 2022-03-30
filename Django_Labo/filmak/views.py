b43d86da9d838225b06d9e3284c9adfdfb420f1d
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-30 12:14:59 +0200

    Login gutxi gora-behera eginda

--- Django_Labo/filmak/views.py
+++ Django_Labo/filmak/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django import forms
+from django.contrib.auth import authenticate, login as auth_login
 
 def index(request):
 
@@ -14,27 +15,57 @@ def index(request):
                   )
 
 def login(request):
-
-    form = LoginForm()
-
-    return render(request, "filmak/login.html",
-                  {
-                      'title' : "Login - Filmak",
-                      'message' : "Hello Django!",
-                      'content' : "Hello, world. You're at the polls index.",
-                      'form' : form
-                      }
-                  )
+    if request.method == 'POST':
+        erabiltzailea = request.POST['erabiltzailea']
+        pasahitza = request.POST['pasahitza']
+        user = authenticate(username=erabiltzailea, password=pasahitza)
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                # Berbideratu login ondorengo orri batera.
+                return HttpResponseRedirect('menua.html')
+            else:
+                # Errore-mezua bueltatu: 'kontua desgaituta'.
+                return render(request, "filmak/login.html",
+                {
+                    'error' : "Kontua desgaituta."
+                    }
+                )
+        else:
+            return render(request, "filmak/login.html",
+            {
+                'error' : "Login desegokia."
+                }
+            )
+    else:
+        form = LoginForm()
+        return render(request, "filmak/login.html",
+                      {
+                          'title' : "Login - Filmak",
+                          'message' : "Hello Django!",
+                          'content' : "Hello, world. You're at the polls index.",
+                          'form' : form
+                          }
+                      )
 
 def register(request):
-
-    return render(request, "filmak/register.html",
-                  {
-                      'title' : "Register - Filmak",
-                      'message' : "Hello Django!",
-                      'content' : "Hello, world. You're at the polls index."
-                      }
-                  )
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = RegisterForm()
+        return render(request, "filmak/register.html",
+                      {
+                          'title' : "Register - Filmak",
+                          'message' : "Hello Django!",
+                          'content' : "Hello, world. You're at the polls index.",
+                          'form' : form
+                          }
+                      )
 
 def logout(request):
 
@@ -77,5 +108,9 @@ def zaleak(request):
                   )
 
 class LoginForm(forms.Form): #Manualki login formularioa
+    erabiltzailea = forms.CharField(max_length=100, required=True) #Erabiltzaile izena
+    pasahitza = forms.CharField(widget=forms.PasswordInput, required=True) #defektuz required beti da TRUE
+
+class RegisterForm(forms.Form): #Manualki login formularioa
     erabiltzailea = forms.CharField(max_length=100, required=True) #Erabiltzaile izena
     pasahitza = forms.CharField(widget=forms.PasswordInput, required=True) #defektuz required beti da TRUE
