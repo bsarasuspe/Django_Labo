34b44f64158f4c27330e1c45e6916429cc1f50de
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-04-06 11:03:16 +0200

    logout redirect

--- Django_Labo/Django_Labo/settings.py
+++ Django_Labo/Django_Labo/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGOUT_REDIRECT_URL = 'index'
+
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = [
